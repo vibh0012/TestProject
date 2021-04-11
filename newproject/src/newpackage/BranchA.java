f4c8848a8540f4ba00384d23be722306b1b7daba
vibh0012 <vibhor.msn@gmail.com>
2021-04-11 22:03:38 +0530

    removing files of a previous package

--- newproject/src/newpackage/BranchA.java
+++
@@ -1,7 +0,0 @@
-package newpackage;
-public class BranchA{
-
-  public static void main(String[] args){
-    System.out.println("You should be in branch a");
-  }
-}
--- newproject/src/newpackage/trrial.java
+++
@@ -1,7 +0,0 @@
-package newpackage;
-public class trrial{
-
-  public static void main(String[] args){
-    System.out.println("Created a new class trrial!");
-  }
-}
